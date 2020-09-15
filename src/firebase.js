597f2a845b8ef01551419ad0e84a688366f5fdcf
박영호 <mellonia.bluerose@gmail.com>
2020-09-15 17:21:06 +0900

    hide firebase config options for github

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBFuW3xGaUYzEt3vDadrd1TU7INgBlxeIM",
-    authDomain: "twitter-is-a-waste-of-time.firebaseapp.com",
-    databaseURL: "https://twitter-is-a-waste-of-time.firebaseio.com",
-    projectId: "twitter-is-a-waste-of-time",
-    storageBucket: "twitter-is-a-waste-of-time.appspot.com",
-    messagingSenderId: "336256584877",
-    appId: "1:336256584877:web:e2ca7b65ff822667184a24"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 export default firebase.initializeApp(firebaseConfig);
