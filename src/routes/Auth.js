9d76a229c38b31ac3235f768af451e2ade90c643
박영호 <mellonia.bluerose@gmail.com>
2020-09-16 22:13:08 +0900

    create account by email, password

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import { authService } from 'fbase';
+import React, { useState } from 'react'
 
 export default function Auth() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
+
+    const onChange = e => {
+        const { target: { name, value }} = e;
+        if (name === 'email') {
+            setEmail(value);
+        } else if (name === 'password') {
+            setPassword(value);
+        }
+    }
+    const onSubmit = async e => {
+        e.preventDefault();
+
+        try {
+            let data;
+            if (newAccount) {
+                // create account
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            } else {
+                // login
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+        } catch (e) {
+            setError(e.message);
+        }
+    }
+
+    const toggleAccount = () => setNewAccount(prev => !prev);
     return (
         <div>
-            Auth
+            <form onSubmit={onSubmit}>
+                <input
+                    name="email"
+                    type="text"
+                    placeholder="Email"
+                    required
+                    value={email}
+                    onChange={onChange}
+                />
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    required
+                    value={password}
+                    onChange={onChange}
+                />
+                <input type="submit" value={newAccount ? "Create Account" : "Sign In"} />
+                {error}
+            </form>
+            <span onClick={toggleAccount}>{newAccount ? "Sign in" : "Create Account" }</span>
+            <div>
+                <button>Continue with google</button>
+                <button>Continue with github</button>
+            </div>
         </div>
     )
 }
