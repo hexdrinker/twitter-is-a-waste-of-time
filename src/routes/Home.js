3eb1b02796c994ce94cda643e5bbfa3c326f76d6
박영호 <mellonia.bluerose@gmail.com>
2020-09-16 23:47:46 +0900

    set logout

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Home() {
+    const [twit, setTwit] = useState("");
+    const onSubmit = e => {
+        e.preventDefault();
+
+    }
+    const onChange = e => {
+        const { target: { value }} = e;
+        setTwit(value);
+    }
     return (
         <div>
-            Home
+            <form onSubmit={onSubmit}>
+                <input value={twit} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="submit" value="twit" />
+            </form>
         </div>
     )
 }
