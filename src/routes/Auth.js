50011b7a58ce924a1c36debae90f15e36fc0dfc9
박영호 <mellonia.bluerose@gmail.com>
2020-09-16 22:20:36 +0900

    set social login

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,4 @@
-import { authService } from 'fbase';
+import { authService, firebaseInstance } from 'fbase';
 import React, { useState } from 'react'
 
 export default function Auth() {
@@ -33,6 +33,17 @@ export default function Auth() {
     }
 
     const toggleAccount = () => setNewAccount(prev => !prev);
+    
+    const onSocialClick = async e => {
+        const { target: { name }} = e;
+        let provider;
+        if (name === 'google') {
+            provider = new firebaseInstance.auth.GoogleAuthProvider();
+        } else if (name === 'github') {
+            provider = new firebaseInstance.auth.GithubAuthProvider();
+        }
+        const data = await authService.signInWithPopup(provider);
+    }
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -57,8 +68,8 @@ export default function Auth() {
             </form>
             <span onClick={toggleAccount}>{newAccount ? "Sign in" : "Create Account" }</span>
             <div>
-                <button>Continue with google</button>
-                <button>Continue with github</button>
+                <button name="google" onClick={onSocialClick}>Continue with google</button>
+                <button name="github" onClick={onSocialClick}>Continue with github</button>
             </div>
         </div>
     )
