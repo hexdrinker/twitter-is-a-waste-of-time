0de95b0ee66c6c9c1979e2beda100a857ffedef4
박영호 <mellonia.bluerose@gmail.com>
2020-09-20 18:15:23 +0900

    realtime twitting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,13 @@
 import { dbService } from 'fbase';
 import React, { useState, useEffect } from 'react'
 
-export default function Home() {
+export default function Home({ userObj }) {
     const [twit, setTwit] = useState("");
     const [twits, setTwits] = useState([]);
 
+    /*
+    실시간 아님, 좀 오래된 방식
+    
     const getTwits = async () => {
         const dbTwits = await dbService.collection("twits").get();
         dbTwits.forEach(document => {
@@ -15,16 +18,25 @@ export default function Home() {
             setTwits(prev => [twitObj, ...prev]);
         });
     }
+    */
     
     useEffect(() => {
-        getTwits();
+        // getTwits();
+        dbService.collection("twits").onSnapshot(snapshot => {
+            const twitArray = snapshot.docs.map(doc => ({ 
+                id: doc.id,
+                ...doc.data()
+            }));
+            setTwits(twitArray);
+        })
     }, []);
 
     const onSubmit = async e => {
         e.preventDefault();
         await dbService.collection("twits").add({
             twit,
-            createdAt: Date.now()
+            createdAt: Date.now(),
+            creatorId: userObj.uid
         });
         setTwit("");
     }
