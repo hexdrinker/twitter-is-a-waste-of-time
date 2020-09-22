67e9398e02311fcc8d4c1d30872eda6590a25d1d
박영호 <mellonia.bluerose@gmail.com>
2020-09-22 22:22:50 +0900

    Updates

---
+++ precache-manifest.683e1aee3aa1e352d08399d90ab11489.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f615c201f2921465be8ac3a33de184",
+    "url": "/twitter-is-a-waste-of-time/index.html"
+  },
+  {
+    "revision": "c9ded30e535c1dd3225e",
+    "url": "/twitter-is-a-waste-of-time/static/css/main.c04b2658.chunk.css"
+  },
+  {
+    "revision": "0a9d20cf2e091d155756",
+    "url": "/twitter-is-a-waste-of-time/static/js/2.517cd68d.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/twitter-is-a-waste-of-time/static/js/2.517cd68d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9ded30e535c1dd3225e",
+    "url": "/twitter-is-a-waste-of-time/static/js/main.8d15bfcc.chunk.js"
+  },
+  {
+    "revision": "ee529a51d6b3f1c9f46c",
+    "url": "/twitter-is-a-waste-of-time/static/js/runtime-main.c7cd2f4f.js"
+  }
+]);
